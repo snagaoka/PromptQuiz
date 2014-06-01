577fd5601ece74a19fdf79e675d0bbc95b0b79e3
Shannon Nagaoka <shannonnagaoka@gmail.com>
2014-05-31 17:08:26 -1000

    made case insensitive

--- prompt_quiz.js
+++ prompt_quiz.js
@@ -2,15 +2,15 @@
 
 	var score = 0;
 
-	var answer1 = prompt("What type of coffee contains the most caffeine?", " ");
+	var answer1 = prompt("What type of coffee contains the most caffeine?", " ").toLowerCase();
 	if (answer1 == "brewed coffee" || answer1 == "brewed" ) {
 		alert("Correct!");
 		var newscore = score+1;
 	} else {
-		alert("Sorry, the correct answer is brewed coffee.", " ");
+		alert("Sorry, the correct answer is brewed coffee.", " ").toLowerCase();
 	}
 
-	var answer2 = prompt("Which coffee roast contains more caffeine, dark roast or light roast?", " ")
+	var answer2 = prompt("Which coffee roast contains more caffeine, dark roast or light roast?", " ").toLowerCase();
 	if (answer2 == "dark roast" || answer2 == "dark" ) {
 		alert("Correct!");
 		var newscore = newscore+1;
@@ -18,23 +18,23 @@
 		alert("Sorry, the correct answer is dark roast.");
 	}
 
-	var answer3 = prompt("Name one of the two most common types of coffee beans.", " ")
-	if (answer3 == "Arabica" || answer3 == "Robusta" ) {
+	var answer3 = prompt("Name one of the two most common types of coffee beans.", " ").toLowerCase();
+	if (answer3 == "arabica" || answer3 == "robusta" ) {
 		alert("Correct!");
 		var newscore = newscore+1;
 	} else {
 		alert("Sorry, the correct answer is Arabica or Robusta.");
 	}
 
-	var answer4 = prompt("Where is the only place to grow coffee in the US?", " ")
-	if (answer4 == "Hawaii" ) {
+	var answer4 = prompt("Where is the only place to grow coffee in the US?", " ").toLowerCase();
+	if (answer4 == "hawaii" ) {
 		alert("Correct!");
 		var newscore = newscore+1;
 	} else {
 		alert("Sorry, the correct answer is Hawaii.");
 	}
 
-	var answer5 = prompt("What is the approximate average amount of coffee consumed per day (in cups)?", " ")
+	var answer5 = prompt("What is the approximate average amount of coffee consumed per day (in cups)?", " ").toLowerCase();
 	if (answer5 == "3 cups" || answer5 == "3") {
 		alert("Correct!");
 		var newscore = newscore+1;
@@ -42,6 +42,6 @@
 		alert("Sorry, the correct answer is 3 cups.");
 	}
 
-	var message = alert("You got " + newscore);
+	var message = alert("You got " + newscore + " out of 5 correct");
 
 })();
